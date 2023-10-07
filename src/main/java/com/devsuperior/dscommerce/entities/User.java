2757c2a69f0d88691c0b15a4385a98d2222f6f89
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2023-10-07 11:00:41 -0300

    dscommerce - order e user

---
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -0,0 +1,76 @@
+package com.devsuperior.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity//indica que a classe vai ser mapeada para o banco (semelhante ao services)
+@Table(name = "tb_user")//indica o nome da tabela
+public class User {
+    @Id//indica que o atributo é a chave primária
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//indica que a chave é auto incrementada
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDate;
+    private String password;
+
+    @OneToMany(mappedBy = "client")//notação de 1 para muitos
+    List<Order> orders = new ArrayList<>();//necessário criar uma lista do tipo do "muitos"
+
+    public User(){}
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDate, String password) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDate = birthDate;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
