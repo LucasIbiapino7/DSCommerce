c4910610f213dcab5247c1fff24b535ab5d424ae
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2023-10-11 10:07:14 -0300

    equals e hashcode

--- src/main/java/com/devsuperior/dscommerce/entities/User.java
+++ src/main/java/com/devsuperior/dscommerce/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Entity//indica que a classe vai ser mapeada para o banco (semelhante ao services)
 @Table(name = "tb_user")//indica o nome da tabela
@@ -78,5 +79,20 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
