8a92c5e9c77cf074c3eab5875610a923df8b8bc8
Lucas Ibiapino <lucas.ibiapino07@gmail.com>
2023-10-12 16:33:26 -0300

    Get all paged

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -4,9 +4,12 @@ import com.devsuperior.dscommerce.dto.ProductDTO;
 import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -21,6 +24,13 @@ public class ProductService {
         Product product = result.get();//pegamos o product do obj optional
         ProductDTO dto = new ProductDTO(product);//instanciamos o product DTO
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> result = repository.findAll(pageable);//pegando no banco todos os produtos e armazenamos numa lista
+        return result.map(x -> new ProductDTO(x));//pegamos todos os produtos da lista e instanciamos
+        //uma lista de products DTO para cada produto
     }
 
 }
